e59406f4a28a4756fc6d94611bfebf771e15712a
Ariana-Panda <a.arianapanda@gmail.com>
2025-04-06 18:39:13 +0300

    Add files via upload

--- Atestat Ariana/script.js
+++ Atestat Ariana/script.js
@@ -11,3 +11,15 @@ window.addEventListener('scroll', function() {
     }
     lastScrollTop = scrollTop;
 });
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
