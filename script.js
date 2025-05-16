7ce1b5d8da0e9a0903c8c0ecb43d0af1c43a14bb
Ariana-Panda <a.arianapanda@gmail.com>
2025-05-16 23:20:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = '-90px';
+    } else {
+        navbar.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+});
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
